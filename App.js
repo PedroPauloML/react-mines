0456a75ceb88b5584bde6cb9f7ba7ff10c6e1d74
Pedro Paulo Marques de Lima <pedropaulomarqz@gmail.com>
2019-05-22 01:31:20 -0300

    Component mine

--- App.js
+++ App.js
@@ -22,6 +22,9 @@ export default class App extends Component {
         <Field opened nearMines={6} />
         <Field opened nearMines={7} />
         <Field opened nearMines={8} />
+        <Field mined />
+        <Field mined opened />
+        <Field mined opened exploded />
       </View>
     )
   }
